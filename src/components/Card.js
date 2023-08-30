5445871ff6e3d17c4e10f451b73b6ac48a4e5dca
Anri <anri0806@hotmail.co.jp>
2023-08-30 11:13:19 -0400

    Added card components

---
+++ src/components/Card.js
@@ -0,0 +1,17 @@
+function Card() {
+    return (
+      <div>
+        <h2>Card</h2>
+        <p>Campaign: </p>
+        <p>Adset: </p>
+        <p>Creative: </p>
+        <p>Spend: </p>
+        <p>Impressions: </p>
+        <p>Clicks: </p>
+        <p>Results: </p>
+      </div>
+    );
+  }
+  
+  export default Card;
+  
---
+++ src/components/CardsContainer.js
@@ -0,0 +1,12 @@
+import Card from "./Card"; 
+
+function CardsContainer() {
+  return (
+    <div>
+      <h1>CardsContainer</h1>
+      <Card />
+    </div>
+  );
+}
+
+export default CardsContainer;
