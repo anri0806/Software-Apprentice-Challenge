05a1249dd9fa86843cdc3ec29d6f2db0170b01c9
Anri <anri0806@hotmail.co.jp>
2023-08-30 16:44:59 -0400

    added sorting feature

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
+import { useState } from "react";
 import "./App.css";
+
 import CardsContainer from "./components/CardsContainer";
 import { useStandardizedData } from "./customHooks/useStandardizedData";
 
 function App() {
+  const [sort, setSort] = useState("All");
+
   const standardizedData = useStandardizedData();
+
+  function handleSort(value) {
+    setSort(value);
+  }
+
+  const sortedList = [...standardizedData].sort((a, b) => {
+    if (sort === "All") {
+      return standardizedData;
+    } else if (sort === "Spend") {
+      return a.spend - b.spend;
+    } else if (sort === "Ascending") {
+      return a.campaign.localeCompare(b.campaign);
+    } else if (sort === "Descending") {
+      return b.campaign.localeCompare(a.campaign);
+    }
+    return standardizedData;
+  });
+
+  // Apply sorted lists
 
   return (
     <div className="App">
       <div className="App">
-        <CardsContainer standardizedData={standardizedData} />
+        <CardsContainer
+          standardizedData={sortedList}
+          sort={sort}
+          onChangeSort={handleSort}
+        />
       </div>
     </div>
   );
--- src/components/CardsContainer.js
+++ src/components/CardsContainer.js
@@ -1,6 +1,7 @@
+import { useState } from "react";
 import Card from "./Card";
 
-function CardsContainer({ standardizedData }) {
+function CardsContainer({ standardizedData, sort, onChangeSort }) {
   const card = standardizedData.map((data) => (
     <Card
       key={`${data.campaign} + _ + ${data.creative} + _ + ${data.platform}`}
@@ -8,7 +9,18 @@ function CardsContainer({ standardizedData }) {
     />
   ));
 
-  return <div>{card}</div>;
+  return (
+    <div>
+      <label>Sort by:</label>
+      <select value={sort} onChange={(e) => onChangeSort(e.target.value)}>
+        <option value="All">All</option>
+        <option value="Spend">Spend</option>
+        <option value="Ascending">Ascending</option>
+        <option value="Descending">Descending</option>
+      </select>
+      {card}
+    </div>
+  );
 }
 
 export default CardsContainer;
